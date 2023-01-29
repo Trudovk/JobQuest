b333100dc479e13a787e36294132efc5547a082c
Trudovk <Trydovk@yandex.ru>
2023-01-30 02:52:13 +0300

    vacancies upd

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -4,3 +4,8 @@ export async function requestCaptcha() {
   const res = await fetch(`${endpoint}/api/requestcaptcha`);
   return await res.json();
 }
+
+export async function requestVacancies() {
+  const res = await fetch(`${endpoint}/api/jobs?page=1&perpage=5`);
+  return await res.json();
+}
